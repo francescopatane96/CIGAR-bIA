01a429c21a46721cc96eb12c3ab90d9cfbf8d84b
Francesco Patane, MSc <francesco.patane@live.it>
2025-12-11 18:25:15 +0100

    Update events.py

--- cigar_bia/events.py
+++ cigar_bia/events.py
@@ -2,55 +2,51 @@ import pandas as pd
 import pysam
 from collections import Counter
 
-def analyze_editing_events(
-    bam_file,
-    chrom,
-    start,
-    end,
-    output_csv,
-    meta_file=None,
-    status=None,
-    window=3
-):
+def analyze_editing_events(bam_file, chrom, start, end, meta_file=None, status=None, window=3):
     """
-    Analizza tutte le reads in una regione e salva in un unico CSV:
-      - start / end della read
-      - se la read è editata
-      - numero totale di basi editate
-      - conteggio I/D/S
-      - se provoca frameshift
-      - lista CIGAR in formato leggibile
+    Analizza reads deduplicate per (barcode, UMI) e rileva editing events.
+    Ora considera I, D e N come modifiche.
+    
+    Restituisce lista di dict con:
+        - name, start, end
+        - cigar
+        - I_count, D_count, N_count
+        - edited (True/False)
+        - edited_bases
+        - frameshift (True se non multiplo di 3)
+        - barcode, umi
     """
-
     start_ext = max(0, start - window)
     end_ext = end + window
 
     barcodes_to_use = None
     if meta_file:
-        meta = pd.read_csv(meta_file)
-        barcodes_to_use = set(meta.loc[meta["status"] == status, "barcode"])
+        meta_df = pd.read_csv(meta_file)
+        barcodes_to_use = set(meta_df.loc[meta_df['status'] == status, 'barcode'])
 
     bam = pysam.AlignmentFile(bam_file, "rb")
-    results = []
+    umi_dict = {}
 
     for read in bam.fetch(chrom, start_ext, end_ext):
-
         if read.is_unmapped or read.cigartuples is None:
             continue
-
         try:
-            bc = read.get_tag("CB")
-            umi = read.get_tag("UB")
+            bc = read.get_tag('CB')
+            umi = read.get_tag('UB')
         except KeyError:
             continue
 
         if barcodes_to_use and bc not in barcodes_to_use:
             continue
 
-        edit_counter = Counter()
-        edited_bases = 0
+        key = (bc, umi)
+        if key in umi_dict:
+            continue  # deduplicazione
+
         cigar_info = []
         ref_pos = read.reference_start
+        edited_bases = 0
+        I_count = D_count = N_count = 0
 
         for op, length in read.cigartuples:
             if op == 0:  # M
@@ -59,43 +55,40 @@ def analyze_editing_events(
             elif op == 1:  # I
                 cigar_info.append(('I', ref_pos, length))
                 edited_bases += length
-                edit_counter['I'] += 1
+                I_count += length
             elif op == 2:  # D
                 cigar_info.append(('D', ref_pos, ref_pos + length))
                 edited_bases += length
-                edit_counter['D'] += 1
+                D_count += length
+                ref_pos += length
+            elif op == 3:  # N (skipped)
+                cigar_info.append(('N', ref_pos, ref_pos + length))
+                edited_bases += length
+                N_count += length
                 ref_pos += length
             elif op == 4:  # S
                 cigar_info.append(('S', ref_pos, length))
-                edited_bases += length
-                edit_counter['S'] += 1
-            elif op == 3:  # N
-                cigar_info.append(('N', ref_pos, ref_pos + length))
                 ref_pos += length
             else:
                 ref_pos += length
 
-        is_edited = edited_bases > 0
-        frameshift = False if edited_bases == 0 else (edited_bases % 3 != 0)
+        frameshift = (edited_bases % 3) != 0
+        edited = edited_bases > 0
 
-        results.append({
-            "barcode": bc,
-            "umi": umi,
-            "read_name": read.query_name,
-            "start": read.reference_start,
-            "end": read.reference_end,
-            "edited": is_edited,
-            "edited_bases": edited_bases,
-            "I_count": edit_counter['I'],
-            "D_count": edit_counter['D'],
-            "S_count": edit_counter['S'],
-            "frameshift": frameshift,
-            "cigar": cigar_info
-        })
+        umi_dict[key] = {
+            'name': read.query_name,
+            'start': read.reference_start,
+            'end': read.reference_end,
+            'cigar': cigar_info,
+            'I_count': I_count,
+            'D_count': D_count,
+            'N_count': N_count,
+            'edited_bases': edited_bases,
+            'edited': edited,
+            'frameshift': frameshift,
+            'barcode': bc,
+            'umi': umi
+        }
 
     bam.close()
-
-    df = pd.DataFrame(results)
-    df.to_csv(output_csv, index=False)
-
-    return df
+    return list(umi_dict.values())
