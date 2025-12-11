9db1f2dad63202fd4ac1cb17327966c32e9137be
Francesco Patane, MSc <francesco.patane@live.it>
2025-12-11 17:28:28 +0100

    Update events.py

--- cigar_bia/events.py
+++ cigar_bia/events.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pysam
 from collections import Counter
 
-def analyze_editing_events(
+def analyze_and_save_editing_events(
     bam_file,
     chrom,
     start,
@@ -13,11 +13,13 @@ def analyze_editing_events(
     window=3
 ):
     """
-    Analizza tutte le reads in una regione, salva in un unico CSV:
+    Analizza tutte le reads in una regione e salva in un unico CSV:
+      - start / end della read
       - se la read è editata
       - numero totale di basi editate
       - conteggio I/D/S
       - se provoca frameshift
+      - lista CIGAR in formato leggibile
     """
 
     start_ext = max(0, start - window)
@@ -29,7 +31,6 @@ def analyze_editing_events(
         barcodes_to_use = set(meta.loc[meta["status"] == status, "barcode"])
 
     bam = pysam.AlignmentFile(bam_file, "rb")
-
     results = []
 
     for read in bam.fetch(chrom, start_ext, end_ext):
@@ -46,23 +47,33 @@ def analyze_editing_events(
         if barcodes_to_use and bc not in barcodes_to_use:
             continue
 
-        # ---- ANALISI CIGAR ----
         edit_counter = Counter()
         edited_bases = 0
+        cigar_info = []
+        ref_pos = read.reference_start
 
         for op, length in read.cigartuples:
-
-            if op == 1:       # Insertion
-                edit_counter["I"] += 1
+            if op == 0:  # M
+                cigar_info.append(('M', ref_pos, ref_pos + length))
+                ref_pos += length
+            elif op == 1:  # I
+                cigar_info.append(('I', ref_pos, length))
                 edited_bases += length
-
-            elif op == 2:     # Deletion
-                edit_counter["D"] += 1
+                edit_counter['I'] += 1
+            elif op == 2:  # D
+                cigar_info.append(('D', ref_pos, ref_pos + length))
                 edited_bases += length
-
-            elif op == 4:     # Soft clipping
-                edit_counter["S"] += 1
+                edit_counter['D'] += 1
+                ref_pos += length
+            elif op == 4:  # S
+                cigar_info.append(('S', ref_pos, length))
                 edited_bases += length
+                edit_counter['S'] += 1
+            elif op == 3:  # N
+                cigar_info.append(('N', ref_pos, ref_pos + length))
+                ref_pos += length
+            else:
+                ref_pos += length
 
         is_edited = edited_bases > 0
         frameshift = False if edited_bases == 0 else (edited_bases % 3 != 0)
@@ -71,17 +82,19 @@ def analyze_editing_events(
             "barcode": bc,
             "umi": umi,
             "read_name": read.query_name,
+            "start": read.reference_start,
+            "end": read.reference_end,
             "edited": is_edited,
             "edited_bases": edited_bases,
-            "I_count": edit_counter["I"],
-            "D_count": edit_counter["D"],
-            "S_count": edit_counter["S"],
-            "frameshift": frameshift
+            "I_count": edit_counter['I'],
+            "D_count": edit_counter['D'],
+            "S_count": edit_counter['S'],
+            "frameshift": frameshift,
+            "cigar": cigar_info
         })
 
     bam.close()
 
-    # ---- Salva tutto in CSV ----
     df = pd.DataFrame(results)
     df.to_csv(output_csv, index=False)
 
